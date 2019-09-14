e123678a23b1bb24e014647a465a6dcd619ae5de
coder004 <30897961+coder004@users.noreply.github.com>
2019-09-14 17:02:17 -0400

    face authentication

---
+++ SecureTravel/SecureTravel/AzureFaceRecognition.swift
@@ -0,0 +1,99 @@
+//
+//  AzureFaceRecognition.swift
+//  SecureTravel
+//
+//  Created by Siddharth on 14/09/19.
+//  Copyright © 2019 Siddharth. All rights reserved.
+//
+
+import UIKit
+
+let APIKey = "353a80555bd2479890cffc6bc492fa47" // Ocp-Apim-Subscription-Key
+let Region = "eastus"
+let FindSimilarsUrl = "https://\(Region).api.cognitive.microsoft.com/face/v1.0/findsimilars"
+let DetectUrl = "https://\(Region).api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true"
+
+class AzureFaceRecognition: NSObject {
+    
+    static let shared = AzureFaceRecognition()
+    
+    func syncDetectFaceIds(imageData: Data) -> String {
+        var headers: [String: String] = [:]
+        headers["Content-Type"] = "application/octet-stream"
+        headers["Ocp-Apim-Subscription-Key"] = APIKey
+        
+        let response = self.makePOSTRequest(url: DetectUrl, postData: imageData, headers: headers)
+        for faceInfo in response {
+            if let faceId = faceInfo["faceId"] as? String  {
+                return faceId
+            }
+        }
+        return ""
+    }
+
+    
+    func findSimilars(currentFaceId: String, userFaceIds: [String], completion: @escaping (Bool) -> Void) {
+        
+        var headers: [String: String] = [:]
+        headers["Content-Type"] = "application/json"
+        headers["Ocp-Apim-Subscription-Key"] = APIKey
+        
+        let params: [String: Any] = [
+            "faceId": currentFaceId,
+            "faceIds": userFaceIds,
+            "mode": "matchFace"
+        ]
+        
+        // Convert the Dictionary to Data
+        let data = try! JSONSerialization.data(withJSONObject: params)
+        
+        DispatchQueue.global(qos: .background).async {
+            let response = self.makePOSTRequest(url: FindSimilarsUrl, postData: data, headers: headers)
+            var authenticated = false
+            let minConfidence = 0.4
+            for info in response {
+//                if let faceId = info["faceId"] as? String  {
+                let confidence = (info["confidence"] as! NSNumber).doubleValue
+                if (confidence >= minConfidence) {
+                    print(confidence)
+                    authenticated = true
+                }
+            }
+            DispatchQueue.main.async {
+                completion(authenticated)
+            }
+        }
+    }
+    
+    // Just a function that makes a POST request.
+    private func makePOSTRequest(url: String, postData: Data, headers: [String: String] = [:]) -> [AnyObject] {
+        var object: [AnyObject] = []
+        
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = "POST"
+        request.httpBody = postData
+        
+        for header in headers {
+            request.addValue(header.value, forHTTPHeaderField: header.key)
+        }
+        
+        // Using semaphore to make request synchronous
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data, let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [AnyObject] {
+                object = json
+            }
+            else {
+                print("ERROR response: \(String(data: data!, encoding: .utf8) ?? "")")
+            }
+            
+            semaphore.signal()
+        }
+        
+        task.resume()
+        _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+        
+        return object
+    }
+}
--- SecureTravel/SecureTravel/ViewController.swift
+++ SecureTravel/SecureTravel/ViewController.swift
@@ -12,9 +12,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        doSomething()
     }
-
+    
+    func doSomething() {
+        let dataInfo1 = AzureFaceRecognition.shared.syncDetectFaceIds(imageData: (UIImage(named: "1")?.jpegData(compressionQuality: 1))!)
+        let dataInfo2 = AzureFaceRecognition.shared.syncDetectFaceIds(imageData: (UIImage(named: "2")?.jpegData(compressionQuality: 1))!)
+        let dataInfo3 = AzureFaceRecognition.shared.syncDetectFaceIds(imageData: (UIImage(named: "3")?.jpegData(compressionQuality: 1))!)
+        let dataInfo4 = AzureFaceRecognition.shared.syncDetectFaceIds(imageData: (UIImage(named: "4")?.jpegData(compressionQuality: 1))!)
+        
+        
+        print(dataInfo1)
+        print(dataInfo2)
+        print(dataInfo3)
+        print(dataInfo4)
+        AzureFaceRecognition.shared.findSimilars(currentFaceId: dataInfo1, userFaceIds: [dataInfo4]) { (authenticated) in
+            print(authenticated)
+        }
+    }
 
 }
 
